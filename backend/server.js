b268e942d63778cb990675cc659ab1863f6b2ee7
Princepreet <Princepreet2021@gmail.com>
2025-08-23 23:37:41 -0400

    deploying?

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,13 @@ import flashcardRoutes from './routes/flashcardRoutes.js'
 import openAiRoutes from './routes/openAiRoutes.js'
 import cookieParser from "cookie-parser";
 import { connectDb } from "./database/connectDb.js";
+import path from 'path';
 import cors from 'cors';
 
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve();
 
 app.use(cors({origin: 'http://localhost:5173', credentials:true}));
 
@@ -18,12 +20,19 @@ app.use(express.json());
 app.use(cookieParser()); //allows us to extract a token
 
 
-
 app.use('/api/auth', authRoutes);
 app.use('/api/decks', flashcardRoutes);
 app.use('/api/flashcards', openAiRoutes);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     connectDb()
-    console.log('running ayyy');
+    console.log("Server is running on port: ", PORT);
 });
